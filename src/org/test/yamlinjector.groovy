313cfff767a2bb22895fbe2c710e027290721f3c
DESKTOP-FFKIS69\Bouda <badereddine.ouerghi@esprit.tn>
2020-06-22 15:01:33 -0400

    no message

---
+++ src/org/test/yamlinjector.groovy
@@ -0,0 +1,13 @@
+package org.test
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.DumperOptions
+import static org.yaml.snakeyaml.DumperOptions.FlowStyle.BLOCK
+
+class yaminjector implements Serializable {
+static def   updateDeployment(String fileName, String imageName){
+    def yaml = readYaml file: $fileName
+    yaml.spec.template.image = $imageName
+    writeFile file:$fileName, text:yamlToString(yaml)
+}
+}
