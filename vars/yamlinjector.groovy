cd4ff883c63f3dce691706e9e859c85925fd35b6
DESKTOP-FFKIS69\Bouda <badereddine.ouerghi@esprit.tn>
2020-06-22 16:02:17 -0400

    no message

--- vars/yamlinjector.groovy
+++ vars/yamlinjector.groovy
@@ -3,11 +3,11 @@ import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.DumperOptions
 import static org.yaml.snakeyaml.DumperOptions.FlowStyle.BLOCK
 def call(def fileName, def imageName) {
-dir('hello-world-war/') {
+
 
 sh 'ls -l'
     def yaml = readYaml(file: "${fileName}")
     yaml.spec.template.image = "${imageName}"
     writeFile file:"${fileName}", text:(yaml.toString())
-}
+
 }
