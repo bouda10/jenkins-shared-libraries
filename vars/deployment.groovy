2bd7ca5421c1c698eed4bc048b7876056a056d0a
DESKTOP-FFKIS69\Bouda <badereddine.ouerghi@esprit.tn>
2020-06-23 13:29:35 -0400

    no message

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -34,7 +34,6 @@ sh 'chmod -R  777 .'
         stage('Maven Build') {
             container('maven') {
                 dir('hello-world-war/') {
-				yamlinjector("bouda-deploy.yaml","nexus.do/bouda:latest")
                     sh 'hostname'
                     sh 'hostname -i'
                     sh 'mvn clean install'
--- vars/yamlinjector.groovy
+++ vars/yamlinjector.groovy
@@ -3,11 +3,12 @@ import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.DumperOptions
 import static org.yaml.snakeyaml.DumperOptions.FlowStyle.BLOCK
 def call(def fileName, def imageName) {
+    script {
 
 
-sh 'ls -l'
-    def yaml = readYaml(file: "${fileName}")
-    yaml.spec.template.image = "${imageName}"
-    writeFile file:"${fileName}", text:(yaml.toString())
-
+        sh 'ls -l'
+        def yaml = readYaml file: "${fileName}"
+        yaml.spec.template.image = "${imageName}"
+        writeFile file: "${fileName}", text: (yaml.toString())
+    }
 }
